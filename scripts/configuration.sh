554d82b20d2fb43a26c38325b34316c193b73203
alex <aangulo@geniuscode.mx>
2024-03-07 20:32:48 -0700

    Add a comment that makes the code easier to read

--- scripts/configuration.sh
+++ scripts/configuration.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Wait before sqlserver is up and running, before creating our database and tables
 sleep 200
 
+# Login to sqlserver and create our database and tables
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -d master -i /usr/config/setup.sql
